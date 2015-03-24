39f474d4538d4641651b1fd847fd74cf8b8c5502
Eden <edenshapiro@gmail.com>
2015-03-24 16:40:14 -0700

    changing favorite things table

--- FavoriteThings/FavoriteThings/ViewController.swift
+++ FavoriteThings/FavoriteThings/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let favoriteThings = [
         //TODO: Fill this array with your favorite things. Then use this collection to populate your table.
+        "firefox",
+        "beatles"
+        
     ]
 
     // Mark: Table View Data Source Methods
@@ -27,7 +30,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       
         // TODO: Implement this method to get the correct row count
-        let placeholderCount = 2
+        let placeholderCount = self.favoriteThings.count
         return placeholderCount
     }
 
